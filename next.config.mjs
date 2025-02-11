c12f2765b059b240baa2ba1972172478d91ec3b9
Emilia González <emilia@serpa.cloud>
2025-02-11 00:06:02 -0600

    feat: optimize images and add material icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.serpa.cloud",
+        port: "",
+        pathname: "/random-image",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
